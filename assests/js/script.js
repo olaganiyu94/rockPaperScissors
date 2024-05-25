47f8bcb6966854aea51e42f8ef710b4f6cded5cc
Ola Ganiyu <ganiyumuftau@mail.com>
2024-05-25 11:14:14 +0000

    images added

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,1 +1,8 @@
-console.log("connected")
+//get the elements
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const container = document.querySelector(".container");
+
+//Button
+const replay = document.getElementById("replay");
